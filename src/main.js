fc37fd782b3987887019fc6c57e0eaa8f3e91474
Chikobara <albaraa.as.2002@gmail.com>
2024-08-29 08:48:38 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
-const path = require("path");
-const url = require("url");
-const isDev = !app.isPackaged;
-
+const { app, BrowserWindow } = require("electron");
 // app.disableHardwareAcceleration();
 
 // Create the main window
@@ -16,34 +12,20 @@ async function createWindow() {
     minHeight: 800,
     autoHideMenuBar: true,
     webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-      enableRemoteModule: true,
-      nodeIntegration: true,
-      // contextIsolation: false,
+      //preload: path.join(__dirname, "preload.js"),
+      //enableRemoteModule: true,
+      nodeIntegration: false,
+      contextIsolation: true,
     },
   });
-
-  if (isDev) {
-    // In development, we'll use the Next.js dev server
-    mainWindow.loadURL("https://open-memoir.vercel.app/documents/");
-    // mainWindow.webContents.openDevTools(); // this will open browser dev tools
-  } else {
-    // In production, we'll load the built app
-    // const startUrl = url.format({
-    //   pathname: path.join(
-    //     "memoir-990afngcz-memoirs-projects.vercel.app/documents/"
-    //   ),
-    //   protocol: "https://",
-    //   slashes: true,
-    // });
     const startUrl = "https://open-memoir.vercel.app/documents/";
     mainWindow.loadURL(startUrl);
-  }
 
   mainWindow.on("closed", function () {
     mainWindow = null;
   });
 }
+//app.commandLine.appendSwitch('disable-gpu');
 app.on("ready", createWindow);
 
 app.on("window-all-closed", function () {
